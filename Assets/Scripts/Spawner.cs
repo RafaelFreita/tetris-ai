33c4ae12c4eede648e3f476de8814bf04d051077
RafaelFreita <dfr.rafael@gmail.com>
2020-11-17 11:18:18 -0300

    Adding spawner; Fixing groups pivots

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    [SerializeField]
+    private List<GameObject> groups = new List<GameObject>();
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			SpawnNext();
+		}
+	}
+
+	private GameObject SpawnNext()
+	{
+        int index = Random.Range(0, groups.Count);
+
+        GameObject spawnedGroup = Instantiate(groups[index], transform.position, Quaternion.identity);
+        return spawnedGroup;
+	}
+
+}
