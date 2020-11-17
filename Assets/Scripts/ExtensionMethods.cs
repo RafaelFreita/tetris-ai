f7af9f258130d4d728ec3029bd6514ba6c8ecc1d
RafaelFreita <dfr.rafael@gmail.com>
2020-11-17 11:40:23 -0300

    Adding Vector2 round extension method

---
+++ Assets/Scripts/ExtensionMethods.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ExtensionMethods
+{
+    public static void Round(this Vector2 v)
+	{
+		v.x = Mathf.RoundToInt(v.x);
+		v.y = Mathf.RoundToInt(v.x);
+	}
+}
